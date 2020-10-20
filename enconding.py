e77c94ffdd93ca9734b32980ebda29941a3aaa47
JonasTadeu023 <jonas.2018304986@alunno.iffar.edu.br>
2020-10-20 10:16:26 -0300

    Limpando a pasta

--- enconding.py
+++
@@ -1,6 +0,0 @@
-import face_recognition
-
-djames     = face_recognition.load_image_file("images_missing/Jonas/Jonas.jpg")
-djames_enc = face_recognition.face_encodings(djames)[0]
-
-print(djames_enc)
